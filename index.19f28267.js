c15d7b1b42b07ddf132b18025f7f345356c2896e
tobias <tobias.harasim@seznam.cz>
2025-05-02 13:13:16 +0200

    make build

---
+++ index.19f28267.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".header__burger"),s=document.querySelector(".header");e.addEventListener("click",function(){e.classList.contains("active")?(e.classList.remove("active"),s.classList.remove("active")):(e.classList.add("reset"),setTimeout(function(){e.classList.remove("reset"),e.classList.add("active"),s.classList.add("active")},400))});
+//# sourceMappingURL=index.19f28267.js.map
